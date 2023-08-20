eac79cf9d14e0c9d3c0595775a73b3296b35e556
Joao Lino <linusmannuel@gmail.com>
2023-08-20 11:18:53 +0100

    arquivo ts

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,4 @@
 import { Negociacao } from './models/negociacao.js';
 const negociacao = new Negociacao(new Date());
 console.log('Volume igual a: ', negociacao.volume);
+negociacao.quantidade = 10;
