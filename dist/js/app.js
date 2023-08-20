9349a636068d14da550192434233e17f7e85b850
Joao Lino <linusmannuel@gmail.com>
2023-08-21 00:00:30 +0100

    integracao com o formulario

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,7 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
-import { Negociacao } from './models/negociacao.js';
-const negociacao = new Negociacao(new Date(), 10, 100);
-const novaNegociacao = new NegociacaoController();
-console.log(novaNegociacao.adiciona());
+const controller = new NegociacaoController();
+const form = document.querySelector('[data-form]');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
