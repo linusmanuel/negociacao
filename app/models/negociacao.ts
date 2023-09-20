1b56ea8b0fd720814a11463afce8d2daa26c6db7
Joao Lino <linusmannuel@gmail.com>
2023-09-20 12:38:10 +0100

    mais sobre readonly

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,16 +1,11 @@
 export class Negociacao {
     constructor(
-        private _data: Date, 
-        private _quantidade: number, 
+        public readonly _data: Date, 
+        public readonly _quantidade: number, 
         private _valor: number
     ) {}
 
     get volume(): number {
         return this._quantidade * this._valor;
-    }
-
-    get data(): Date {
-        const data = new Date(this._data.getTime());
-        return data;
     }
 }
